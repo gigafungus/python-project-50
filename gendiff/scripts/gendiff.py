f1eaf74a2a3afb434e01709fc4685e965f0c0be9
fungus <amortis@inbox.ru>
2026-01-28 13:02:25 +0300

    edit gendiff, now parses files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,17 @@
 import argparse
+import json
 
 
 def main():
     gendiff_parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
+        description="Compares two configuration files and shows a difference.",
     )
     gendiff_parser.add_argument("first_file")
     gendiff_parser.add_argument("second_file")
     gendiff_parser.add_argument("-f", "--format", help="set format of output")
     args = gendiff_parser.parse_args()
-    return args
+    print(json.load(open(args.first_file)))
+    print(json.load(open(args.second_file)))
 
 
 if __name__ == "__main__":
