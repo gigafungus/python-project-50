4e72a8b5ba335cd36f3da38f769a04cc76343a6a
fungus <amortis@inbox.ru>
2026-01-28 19:29:40 +0300

    feat: add func that returs diff of two .json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,8 +10,33 @@ def main():
     gendiff_parser.add_argument("second_file")
     gendiff_parser.add_argument("-f", "--format", help="set format of output")
     args = gendiff_parser.parse_args()
-    print(json.load(open(args.first_file)))
-    print(json.load(open(args.second_file)))
+    f1 = json.load(open(args.first_file))
+    f2 = json.load(open(args.second_file))
+    print(generate_diff(f1, f2))
+
+
+def generate_diff(f1, f2):
+    result = {}
+    if isinstance(f1, str):
+        f1 = json.load(open(f1))
+        f2 = json.load(open(f2))
+    for k, v in sorted(f1.items()):
+        if k not in f2.keys():
+            result[f"- {k}"] = v
+        elif f1[k] == f2[k]:
+            result[f"  {k}"] = v
+        else:
+            result[f"- {k}"] = v
+            result[f"+ {k}"] = f2[k]
+
+    for key in filter(lambda k: k not in f1.keys(), f2.keys()):
+        result[f"+ {key}"] = f2[key]
+
+    return (
+        "{\n"
+        + "\n".join(f"{k}: {json.dumps(v).strip('\'"')}" for k, v in result.items())
+        + "\n}"
+    )
 
 
 if __name__ == "__main__":
