30c88d677ff187377b98948cb5d7458c43671292
fungus <amortis@inbox.ru>
2026-02-02 18:55:32 +0300

    add test for yaml files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,9 +11,17 @@ def read_file(filename):
     return get_test_data(filename).read_text()
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     file_1 = get_test_data("alpha.json")
     file_2 = get_test_data("beta.json")
     expected = read_file("expected.txt")
 
     assert generate_diff(file_1, file_2) == expected
+
+
+def test_generate_diff_yaml():
+    file1 = get_test_data("alpha.yml")
+    file2 = get_test_data("beta.yaml")
+    expected = read_file("expected.txt")
+
+    assert generate_diff(file1, file2) == expected
