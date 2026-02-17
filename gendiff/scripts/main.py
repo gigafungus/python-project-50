8c130382b317cc6a5217ff3e2104ff83a05fe4e9
fungus <amortis@inbox.ru>
2026-02-17 21:36:50 +0300

    added default format arg

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -9,11 +9,14 @@ def main():
     )
     gendiff_parser.add_argument("first_file")
     gendiff_parser.add_argument("second_file")
-    gendiff_parser.add_argument("-f", "--format", help="set format of output")
+    gendiff_parser.add_argument(
+        "-f", "--format", default="stylish", help="set format of output"
+    )
     args = gendiff_parser.parse_args()
     file_1 = args.first_file
     file_2 = args.second_file
-    print(generate_diff(file_1, file_2))
+    format_name = args.format
+    print(generate_diff(file_1, file_2, format_name))
 
 
 if __name__ == "__main__":
